bb967d09bc06a7943799e68d639c4b010e8bdc48
WarlOrd <tycoon111222333@gmail.com>
2025-02-21 02:06:20 +0300

    fixes

--- solution/internal/adapters/database/postgres/ads.go
+++ solution/internal/adapters/database/postgres/ads.go
@@ -109,7 +109,7 @@ WHERE CASE
   AND NOT EXISTS (SELECT 1
                   FROM impressions i
                   WHERE i.campaign_id = c.id
-                    AND i.client_id = $1);
+                    AND i.client_id = $1)
 `
 
 type GetEligibleAdsParams struct {
