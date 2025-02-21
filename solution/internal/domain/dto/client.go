41a249efc669b5984d79a6a035dee21c025775e8
WarlOrd <tycoon111222333@gmail.com>
2025-02-21 17:33:50 +0300

    oijdfllksjdmflks

--- solution/internal/domain/dto/client.go
+++ solution/internal/domain/dto/client.go
@@ -2,7 +2,7 @@ package dto
 
 type CreateClientDTO struct {
 	ClientId string `json:"client_id" validate:"required,uuid"`
-	Login    string `json:"login" validate:"required"`
+	Login    string `json:"login" validate:"required,email"`
 	Age      int    `json:"age" validate:"required"`
 	Location string `json:"location" validate:"required"`
 	Gender   string `json:"gender" validate:"required"`
